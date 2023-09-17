aa6b5a5e3b259493b4bf65bb05c78fa2dbd5cb74
bairoseBanu <119371152+bairoseBanu@users.noreply.github.com>
2023-09-17 20:33:26 +0100

    changed comments

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require("express");
+// const express = require("express");
+// const mongodb = require('mongodb');
+// const url = 'mongodb://localhost:27017';
+// const MongoClient = mongodb.MongoClient;
+// const client = new MongoClient(url);
+// const app = express();
+//const PORT=3000;
+
+// app.post("/hello", async (req,res)=>{
+//      await client.connect();
+
+
+// })
+
+// app.listen(PORT,()=>{
+//     console.log(`Server is running ${PORT}`)
+// });
+const express = require('express');
 
 const app = express();
-const PORT=3000;
+const PORT =(3000);
 
-app.get("/", (req,res)=>{
-    res.send(`welcome to the server from ${PORT} `)
-})
+
+app.get("/",(req,res)=>{
+   res.send('welcome to the server from port $ {PORT}')
+});
 
 app.listen(PORT,()=>{
-    console.log(`Server is running ${PORT}`)
+    console.log('server is running at port ${PORT}')
 });
+
